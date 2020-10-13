bb33a0e2b136f39331e51956f2834bf9106d48de
DivyanshArya23 <divyansharya20@gmail.com>
2020-10-13 15:53:39 +0530

    courses dropdown in navbar: hover fixed

---
+++ custom/dropdown.js
@@ -0,0 +1,8 @@
+function toggleDropDown(e){
+    let menu=document.getElementById('dd-menu')
+    if( menu.style.display!=="block"){
+        menu.style.display='block';
+    } else {
+        menu.style.display='none';
+    }
+}
